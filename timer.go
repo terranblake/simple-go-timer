a4465a703ec029e117acc609e66e2fd67afac65c
Terran Blake <terranblake@gmail.com>
2020-01-24 16:24:08 -0800

    add .gitignore for binaries and use functions

--- timer.go
+++ timer.go
@@ -17,7 +17,7 @@ func main() {
 	args := os.Args[1:]
 
 	// convert the first argument to a 64-bit integer
-	amountOfTime, parseErr := strconv.ParseInt(args[0], 10, 64)
+	duration, parseErr := strconv.ParseInt(args[0], 10, 64)
 
 	// if there's an issue parsing the string to an integer
 	// handle the error here
@@ -29,12 +29,16 @@ func main() {
 	// and assign to this title variable
 	title := strings.Join(args[1:], " ")
 
+	createTimer(duration, title)
+}
+
+func createTimer(duration int64, title string) {
 	// give feedback to the user that something is working
-	fmt.Println("i will notify you of \"", title, "\" in", amountOfTime, "seconds")
+	fmt.Println("timer set for", duration, "s for event \""+title+"\"")
 
 	// convert the int64 to a item.Duration and cast it to
 	// a time.Second just before sleeping for the given duration
-	time.Sleep(time.Duration(amountOfTime) * time.Second)
+	time.Sleep(time.Duration(duration) * time.Second)
 
 	// notify the user with the title provided
 	// todo: bundle the application and use the
