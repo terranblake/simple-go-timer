e520ff70e5a3828ec98844c4974d3372f9ac7ba1
Terran Blake <terranblake@gmail.com>
2020-01-24 16:37:00 -0800

    output remaining time for timer before notifying

--- timer.go
+++ timer.go
@@ -17,7 +17,7 @@ func main() {
 	args := os.Args[1:]
 
 	// convert the first argument to a 64-bit integer
-	duration, parseErr := strconv.ParseInt(args[0], 10, 64)
+	duration, parseErr := strconv.Atoi(args[0])
 
 	// if there's an issue parsing the string to an integer
 	// handle the error here
@@ -32,13 +32,14 @@ func main() {
 	createTimer(duration, title)
 }
 
-func createTimer(duration int64, title string) {
+func createTimer(duration int, title string) {
 	// give feedback to the user that something is working
-	fmt.Println("timer set for", duration, "s for event \""+title+"\"")
-
-	// convert the int64 to a item.Duration and cast it to
-	// a time.Second just before sleeping for the given duration
-	time.Sleep(time.Duration(duration) * time.Second)
+	// in this case, we are using a timer to update the console
+	// every second with the remaining seconds in the timer
+	for i := 0; i < duration; i++ {
+		fmt.Print("\rtimer set for ", (duration - i), "s from now for event \""+title+"\"")
+		time.Sleep(time.Second)
+	}
 
 	// notify the user with the title provided
 	// todo: bundle the application and use the
